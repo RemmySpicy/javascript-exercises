2391512718a568efb2f90f6b66196a5e7c04f6c0
O.Remilekun <remmylknolaotan@gmail.com>
2022-07-14 20:11:01 +0100

    Solve the removeFromArr challenge

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    
+    // Method 1
+    // This array contains strings that needs to be removed from main array
+    let removeItem = [...args];
+    return arr.filter( val => removeItem.indexOf(val) < 0)
+    
+    // Mathod 2
+    // let localArr = [...arr];
+    // for (let i = arr.length; i >= 0; i--) {
+    //     for (let j = 0; j < args.length; j++) {
+    //         if (arr[i] === args[j]) {
+    //             localArr.splice(i, 1)
+    //         }
+    //     }
+    // }
+    // return localArr;
 };
 
 // Do not edit below this line
