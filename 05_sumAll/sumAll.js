4c4c2fa791def3c76136c4c7a00d2945f7f130e2
O.Remilekun <remmylknolaotan@gmail.com>
2022-07-14 09:43:48 +0100

    Remove isNaN if..else condition

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,8 @@
 const sumAll = function(n1, n2) {
 let sum = 0;
-if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+if ((typeof n1 !== 'number' || typeof n2 !== 'number') ||
+    (n1 < 0 || n2 < 0)) {
     return 'ERROR';
-    } else if ((isNaN(n1) || isNaN(n2)) || (n1 < 0 || n2 < 0)) {
-        return 'ERROR';
     } else if (n1 > n2) {
         for (let i = n2; i <= n1; i++) {
             sum += i;
